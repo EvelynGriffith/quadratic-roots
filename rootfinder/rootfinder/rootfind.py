058bd58c2ce69fc404b099a6ab1b58ed41968b6c
EvelynGriffith <griffith01@allegheny.edu>
2022-03-01 12:19:38 -0500

    adding to calculate_quadratic_equation_roots

--- rootfinder/rootfinder/rootfind.py
+++ rootfinder/rootfinder/rootfind.py
@@ -8,12 +8,16 @@ def calculate_quadratic_equation_roots(
     a: float, b: float, c: float
 ) -> Tuple[Union[float, complex], Union[float, complex]]:
     """Calculate the roots of a quadratic equation."""
-    # TODO: Use the source code example on page 21 of the
+    #  Use the source code example on page 21 of the
     # "Doing Math with Python" to implement this function.
-    # TODO: Your function should take as input the values
+    D = (b * b - 4 * a * c) ** 0.5
+    # Your function should take as input the values
     # of a, b, and c and return the values of x_one
     # and x_two as a two-tuple of values.
-    # TODO: The return values of this function can either
+    x_one = (-b + D) / (2 * a)
+    x_two = (-b - D) / (2 * a)
+    return x_one, x_two
+    # The return values of this function can either
     # be a complex number (i.e., involving an imaginary
     # number and designated with j) or a floating-point
     # value (i.e., not involving an imaginary number and
